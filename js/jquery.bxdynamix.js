4692ac3fbfbd705edcd587f1f99aeb410ad86fda
Martin Ekelund <martin@kidkie.se>
2014-09-03 23:18:41 +0200

    Update jquery.bxdynamix.js
    Adding support for multiple galleries on same page.

--- js/jquery.bxdynamix.js
+++ js/jquery.bxdynamix.js
@@ -1,5 +1,5 @@
 /**!
- * BX Dynamix v1 - Make built in Wordpress a slideshow by hostile takeover.
+ * BX Dynamix v1.2 - Make built in Wordpress a slideshow by hostile takeover.
  * https://github.com/Kidkie/bx-dynamix
  *
  * Copyright 2014, Martin Ekelund - http://www.martinekelund.com
@@ -8,75 +8,62 @@
  */
 
 jQuery(document).ready(function($){
-
 	// Code to make a slideshow of Wordpress Gallery
-	// function. This utilizes bxSlider as slideshow
+	// function. This utilizes bxSlider as slideshow.
 	if ($('.gallery').length > 0) {
-		var galleryId = $('.gallery').attr('id');
-		var newGallery = "#"+galleryId;
-		var pagerNum = $(galleryId+" li").length;
+		$( ".gallery" ).each(function( index ) {
 
-		// Check that extra tags added by Wordpress that mess up the slideshow is removed.
+			var galleryId = $(this).attr('id');
+			var galleryNew = "#"+galleryId;
+			var pagerNum = $(galleryId+" li").length;
 
-		$(newGallery).find('br').remove();
+			// Check that extra tags added by Wordpress who mess up the slideshow is removed.
+			$(galleryNew).find('br').remove();
 
-		// Please prepare the size of the images.
+			// Please prepare the size of the images.
+			var size_array = Array();
+			$(this).find('dl').each(function() {
+				size = $('img', this).attr('height');
+				size_array.push( size );
+			});
+			var biggest = Math.max.apply( null, size_array );
 
-		var tn_array = Array();
-		var numbers_array = Array();
-		$('.gallery > dl').each(function() {
-			size = $('img', this).attr('height');
-			numbers_array.push( size );
-		});
-		var biggest = Math.max.apply( null, numbers_array );
+			// Get the images, put them as backround and then remove the original.
+			$(this).find('dl').each(function() {
+			    image = $('img', this).attr('src');
+			    size = $('img', this).attr('height');
+			    
+			    // Pick up the tallest image and make the slideshow fit based on that #.
+				$('.gallery-item').css('height',biggest)
+			    
+				// I want my images to center in case they're different height.
+				// This code extracts the image and adds it as a background.
+			    $(this).css('background-image', 'url(' + image + ')');
+			    $(this).css('background-repeat', 'no-repeat');
+			    $(this).css('background-position', 'center center');
+			    $(this).find('dt').remove();
+			});
 
-		// Get the images, put them as backround and then remove the original.
-		
-		$('.gallery > dl').each(function() {
-		    image = $('img', this).attr('src');
-		    size = $('img', this).attr('height');
-		    
-		    // Pick up the tallest image and make the slideshow fit based on that #.
-			$('.gallery-item').css('height',biggest)
-		    
-			// I want my images to center in case they're different height.
-			// This code extracts the image and adds it as a background.
+			// Setup bxSlider options. Find more at http://bxslider.com.
+			$(galleryNew).bxSlider({
+				mode: 'horizontal',
+				captions: true,
+				touchEnabled: true,
+				autoHover: true,
+				useCSS: false,
 
-		    // Options:
-
-		    // Option1: This adds a img-tag instead of putting image in background.
-		    // good for responsive design. Add styles for handling captions in CSS-file.
-		    // $(this).append('<img src="'+image+'" />');
-		    
-		    // Option 2: This part puts image as background instead of img-tag.
-		    $(this).css('background-image', 'url(' + image + ')');
-		    $(this).css('background-repeat', 'no-repeat');
-		    $(this).css('background-position', 'center center');
-		    
-		    $(this).find('dt').remove();
-		});
-
-		// Setup bxSlider option. Find more options at http://bxslider.com.
-
-		$(newGallery).bxSlider({
-			mode: 'horizontal',
-			captions: true,
-			touchEnabled: true,
-			autoHover: true,
-			useCSS: false,
-
-			// Code below uses bxSlider code the add a active class to 
-			// current slide visivle in the viewport
-			onSlideAfter: function (currentSlideNumber, totalSlideQty, currentSlideHtmlObject) {
-				// Outout slide # to console for debugging.
-			    //console.log(currentSlideHtmlObject);
-			    $('.active-slide').removeClass('active-slide');
-			    $(newGallery+'>dl').eq(currentSlideHtmlObject + 1).addClass('active-slide')
-			},
-			onSliderLoad: function () {
-			    $(newGallery+'>dl').eq(1).addClass('active-slide')
-			},
-
+				// Code below uses bxSlider code the add a active class to 
+				// current slide visivle in the viewport
+				onSlideAfter: function (currentSlideNumber, totalSlideQty, currentSlideHtmlObject) {
+					// Outout slide # to console for debugging.
+				    // console.log(currentSlideHtmlObject);
+				    $('.active-slide').removeClass('active-slide');
+				    $(galleryNew).find('dl').eq(currentSlideHtmlObject + 1).addClass('active-slide');
+				},
+				onSliderLoad: function () {
+				    $(galleryNew).find('dl').eq(1).addClass('active-slide');
+				},
+			});
 		});
 	}
 
